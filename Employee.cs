1316d9001cce3816a703060c4a912e0f77375ea8
kishan kumar <kishan.kumarmn@gmail.com>
2022-04-11 12:37:25 +0530

    Committed Employee.cs class file and LitwareLib.cs class file to EMS for Litware Organization project

---
+++ Employee.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMSForLitwareOrganization
+{
+    internal class Employee
+    {
+        private int EmpNo;       //non-static - global vars
+        private String EmpName;
+        private double Salary;
+        private double HRA;
+        private double TA;
+        private double DA;
+        private double PF;
+        private double TDS;
+        private double NetSalary;
+        private double GrossSalary;
+
+        /*public Employee(int EmpNo, String EmpName, double Salary)
+        {
+            this.EmpNo = EmpNo;
+            this.EmpName = EmpName; 
+            this.Salary = Salary;
+            //Console.WriteLine("Enter your Employee Number/Emp ID");
+            //Console.ReadLine();
+        }*/
+        public Employee()  //Zero argument constructor for future reference
+        {
+
+        }
+
+        public int EmployeeNumber { get; set; }
+        public String EmployeeName { get; set; }
+        public double EmployeeSalary { get; set; }
+        public void Method1(int EmpNo)
+        {
+            Console.WriteLine("Enter your Employee Number");
+            Console.ReadLine();
+            if (EmpNo == 101)
+            {
+                this.EmpNo = EmpNo;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Employee Number/ EmpNo Does not exists in the database..");
+            }
+            
+        }
+        public void Method2(String EmpName)
+        {
+            Console.WriteLine("Enter your Employee Name");
+            Console.ReadLine();
+            if(EmpName == "kishan kumar")
+            {
+                this.EmpName = EmpName;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Emloyee Name");
+            }
+        }
+        public void Method3()
+        {
+            Console.WriteLine("Enter your Employee Salary");
+            Console.ReadLine();
+            MethodforHRA();              //calling non-static methods inside non- static method--> just by calling its member name
+            MethodForGrossSalary();      //calling non-static methods inside non- static method--> just by calling its member name
+            CalculateSalary();           //calling non-static methods inside non- static method--> just by calling its member name
+
+            /*if(Salary == 25000)
+            {
+                this.Salary = Salary;
+            }
+            else if(Salary <= 25000)
+            {
+                Console.WriteLine("Mention Exact salary for Employee Kishan kumar ");
+            }
+            else if(Salary >= 25000)
+            {
+                Console.WriteLine("Mention Exact salary for Employee Kishan kumar ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Salary Numbers, Please Enter Valid Salary details");
+            }*/
+        }
+        public void MethodforHRA()
+        {
+            
+            if(Salary < 5000)
+            {
+                HRA = (10 / Salary) * 100;
+                Console.WriteLine(HRA);
+                TA = (5/Salary) * 100;
+                Console.WriteLine(TA);
+                DA = (15/Salary) * 100;
+                Console.WriteLine(DA);
+            }
+            if(Salary < 10000)
+            {
+                HRA = (15 / Salary) * 100;
+                Console.WriteLine(HRA);
+                TA = (10 / Salary) * 100;
+                Console.WriteLine(TA);
+                DA = (20 / Salary) * 100;
+                Console.WriteLine(DA);
+            }
+            if(Salary < 15000)
+            {
+                HRA = (20 / Salary) * 100;
+                Console.WriteLine(HRA);
+                TA = (15 / Salary) * 100;
+                Console.WriteLine(TA);
+                DA = (25 / Salary) * 100;
+                Console.WriteLine(DA);
+            }
+            if(Salary < 20000)
+            {
+                HRA = (25 / Salary) * 100;
+                Console.WriteLine(HRA);
+                TA = (20 / Salary) * 100;
+                Console.WriteLine(TA);
+                DA = (30 / Salary) * 100;
+                Console.WriteLine(DA);
+            }
+            if(Salary >= 20000)
+            {
+                HRA = (30 / Salary) * 100;
+                Console.WriteLine(HRA);
+                TA = (25 / Salary) * 100;
+                Console.WriteLine(TA);
+                DA = (35 / Salary) * 100;
+                Console.WriteLine(DA);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Salary Numbers");
+            }
+        }
+
+        public void MethodForGrossSalary()
+        {
+            Console.WriteLine("Your Gross Salary will be replicated like this");
+            GrossSalary = Salary + HRA + TA + DA;
+            Console.WriteLine(GrossSalary);
+        }
+        public void CalculateSalary()
+        {
+            PF = (10 / GrossSalary) * 100;
+            Console.WriteLine(PF);
+            TDS = (18 / GrossSalary) * 100;
+            Console.WriteLine(TDS);
+            NetSalary = (GrossSalary - (PF + TDS));
+            Console.WriteLine(NetSalary);
+        }
+
+    }
+}
---
+++ LitwareLib.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMSForLitwareOrganization
+{
+    internal class LitwareLib
+    {
+
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            //Employee emp = new Employee(101,"kishan kumar",25025.00);
+            emp.Method1(101);
+            emp.Method2("kishan kumar");
+            emp.Method3();    //this method contains all three implemented methods
+            //emp.MethodforHRA();
+            //emp.MethodForGrossSalary();
+            //emp.CalculateSalary();
+        }
+    }
+}
